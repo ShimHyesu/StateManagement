bfc13a3a275776ede318fa2c8b9bb0b6dfd08a99
Hyesu <hesushim@gmail.com>
2022-07-31 16:55:22 +0900

    사용자 인증 기능 구현 - auth 모듈 작성

---
+++ slices/auth.js
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+// 추후 다른 곳에서 불러와야 하기 때문에 export
+export const User = {
+  id,
+  username,
+  displayName,
+};
+
+const AuthState = {
+  user,
+};
+
+const initialState = {
+  user: null,
+};
+
+// 이 slice에는 두가지 액션 - 사용자 정보를 상태에 담는 authorize 액션, 사용자 정보를 초기화하는 logout 액션
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    authorize(state, action) {
+      state.user = action.payload;
+    },
+    logout(state) {
+      // 업데이트하는 과정에서 action을 사용하지 않으면 생략 가능
+      state.user = null;
+    },
+  },
+});
+
+export default authSlice.reducer;
+export const { authorize, logout } = authSlice.actions;
