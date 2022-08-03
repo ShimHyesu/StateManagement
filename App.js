744ab779d3cee44112328a15bbf2878be3bf4376
Hyesu <hesushim@gmail.com>
2022-08-03 21:52:34 +0900

    리덕스 미들웨어 사용하여 REST API 요청 상태 관리 - PostsApp 틀 잡아주기

--- App.js
+++ App.js
@@ -7,7 +7,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./slices";
 
 import TodoApp from "./components/todo/TodoApp";
+import PostsApp from "./components/post/PostsApp";
 
+// configureStore에는 thunk 미들웨어를 적용하는 것이 내장되어 있으므로
+// 이를 불러오는 작업 생략 가능
 const store = configureStore({
   reducer: rootReducer,
   // 직렬화 할 수 없는 데이터를 수락할 경우
@@ -20,7 +23,7 @@ const store = configureStore({
 function App() {
   return (
     <Provider store={store}>
-      <TodoApp />
+      <PostsApp />
     </Provider>
   );
 }
---
+++ components/post/PostsApp.js
@@ -0,0 +1,67 @@
+//
+// API를 요청한 뒤 화면에 정보를 보여줄 컴포넌트
+//
+import React from "react";
+import {
+  ActivityIndicator,
+  Button,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+} from "react-native";
+
+function PostsApp() {
+  const data = [
+    { id: 1, title: "Hello" },
+    { id: 2, title: "World" },
+  ];
+
+  return (
+    <SafeAreaView style={styles.block}>
+      {data ? (
+        <FlatList
+          style={styles.list}
+          data={data}
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+              <Text>{item.title}</Text>
+            </View>
+          )}
+          keyExtractor={(item) => item.id.toString()}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          ListFooterComponent={() => <View style={styles.separator} />}
+        />
+      ) : (
+        <ActivityIndicator size="large" color="black" style={styles.loading} />
+      )}
+      <Button title="새로고침" onPress={() => {}} />
+    </SafeAreaView>
+  );
+}
+
+export default PostsApp;
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+
+  list: {
+    flex: 1,
+  },
+
+  loading: {
+    flex: 1,
+  },
+
+  item: {
+    padding: 8,
+  },
+
+  separator: {
+    height: 1,
+    backgroundColor: "black",
+  },
+});
